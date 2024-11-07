2104111c046644da1ae7948f56afd6ac5f6c2978
Whoopee949 <hadyzq@outlook.com>
2024-11-07 10:29:26 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+
+print("hello smje4383")
+
+a = 4
+
+if(a == 2):
+    print("True")
+else:
+    print("False")
+
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
+b = "Hello, World!"
+
+# calling function
+b = b.upper()
+print(b)
